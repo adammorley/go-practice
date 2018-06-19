ec3b918c02fcda7921db2b5345b6f427a7b30eaf
Adam Morley <adam@Adams-Mac-mini.local>
2018-06-19 15:33:38 -0700

    add some stuff

---
+++ channel-practice.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+        "crypto/sha1"
+        "fmt"
+        "time"
+)
+
+func hasher(i <-chan []byte, o chan<- [20]byte) {
+    for b := range i {
+        o <- sha1.Sum(b)
+        time.Sleep(100 * time.Millisecond)
+    }
+    close(o)
+}
+
+func filler(c chan<- []byte) {
+    b := make([]byte, 4, 4)
+    for i := 0; i < 10; i++ {
+        for j := 0; j < 4; j++ {
+            b[j] = byte(i)
+        }
+        c <- b
+    }
+    close(c)
+}
+
+func printer(i <-chan [20]byte, d chan<- bool) {
+    for x := range i {
+        fmt.Printf("%x\n", x)
+    }
+    d <- true
+}
+
+func main() {
+    c := make(chan []byte)
+    b := make(chan [20]byte)
+    go hasher(c, b)
+    go filler(c)
+
+    d := make(chan bool)
+    go printer(b, d)
+
+    <-d
+}
