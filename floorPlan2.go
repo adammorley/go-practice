0d12b22a26c47f4fabee3ddf7c8a30c5366c0d99
Adam Morley <adam@Adams-Mac-mini.local>
2018-09-19 13:52:40 -0700

    updates

--- floorPlan2.go
+++ floorPlan2.go
@@ -1,3 +1,4 @@
+// find the distance from any room to the nearest guard, considering locked rooms
 package main
 
 import (
@@ -10,8 +11,9 @@ import (
 const GUARD = -2
 const LOCKED = -1
 
-// row column; maxes are 1-indexed
+// maxes are 1-indexed
 type grid struct {
+    // [row][colum]
 	dist       [][]int
 	cMax, rMax int
 }
@@ -19,6 +21,7 @@ type room struct {
 	r, c int
 }
 
+// create a new grid of arbitrary size
 func newGrid(rMax, cMax int) *grid {
 	var g *grid = new(grid)
 	g.dist = make([][]int, rMax)
@@ -29,24 +32,46 @@ func newGrid(rMax, cMax int) *grid {
 	g.rMax = rMax
 	return g
 }
+// is a room in a grid?
+func (g *grid) contains(r room) bool {
+	if r.r >= 0 && r.r < g.rMax && r.c >= 0 && r.c < g.cMax {
+		return true
+	}
+	return false
+}
+// is a room valid
+func (g *grid) validRoom(r room) {
+    if !g.contains(r) {
+        panic(fmt.Sprintf("room{%d, %d} is not a valid room\n", r.r, r.c))
+    }
+}
+// mark a room as a guard
 func (g *grid) placeGuard(r room) {
+    g.validRoom(r)
 	g.dist[r.r][r.c] = GUARD
 }
+// is a room guarded?
 func (g *grid) guarded(r room) bool {
+    g.validRoom(r)
 	if g.dist[r.r][r.c] == GUARD {
 		return true
 	}
 	return false
 }
+//lock a room
 func (g *grid) lockRoom(r room) {
+    g.validRoom(r)
 	g.dist[r.r][r.c] = LOCKED
 }
+//is a room locked?
 func (g *grid) locked(r room) bool {
+    g.validRoom(r)
 	if g.dist[r.r][r.c] == LOCKED {
 		return true
 	}
 	return false
 }
+// find all the rooms marked as guards in the distance map
 func (g *grid) findGuards() []room {
 	var guards []room = make([]room, 0)
 	for r := 0; r < g.rMax; r++ {
@@ -77,12 +102,6 @@ func (g *grid) String() string {
 	}
 	return sb.String()
 }
-func (g *grid) contains(r room) bool {
-	if r.r >= 0 && r.r < g.rMax && r.c >= 0 && r.c < g.cMax {
-		return true
-	}
-	return false
-}
 func proposeNeighbors(r room) []room {
 	return []room{room{r.r - 1, r.c}, room{r.r, r.c - 1}, room{r.r + 1, r.c}, room{r.r, r.c + 1}}
 }
@@ -98,6 +117,8 @@ func (g *grid) neighbors(r room) []room {
 	}
 	return n
 }
+// for each guard, visit all the rooms and mark their distance.  track which rooms have been visited
+// for any given guard, to prevent multiple-visitation
 func (g *grid) visitRoomsViaGuards(guards []room) {
 	for gI := range guards {
 		// track whether this room has been visited yet
@@ -106,6 +127,7 @@ func (g *grid) visitRoomsViaGuards(guards []room) {
 		g.calcDistances(guard, g.neighbors(guard), visited)
 	}
 }
+// calculate the distances from a guard to a set of rooms
 func (g *grid) calcDistances(guard room, rooms []room, visited map[room]bool) {
 	if len(rooms) != 0 {
 		for r := range rooms {
@@ -114,11 +136,12 @@ func (g *grid) calcDistances(guard room, rooms []room, visited map[room]bool) {
 
 	}
 }
+// calculate the distance from a guard to a room by inspecting the rooms neighbors.  skip visited rooms
 func (g *grid) calcDistance(guard room, r room, visited map[room]bool) {
-	fmt.Println("examining room", r, "from guard location", guard)
 	if visited[r] {
 		return
 	}
+    fmt.Println("visiting room", r, "from guard location", guard)
 	var neighbors []room = g.neighbors(r)
 	if abs(guard.r-r.r)+abs(guard.c-r.c) == 1 { //they are neighbors
 		g.dist[r.r][r.c] = 1
@@ -128,8 +151,10 @@ func (g *grid) calcDistance(guard room, r room, visited map[room]bool) {
 			n := neighbors[i]
 			if g.dist[n.r][n.c] > 0 && g.dist[n.r][n.c] < low {
 				low = g.dist[n.r][n.c]
+                fmt.Println(low)
 			}
 		}
+        // ensure only to pick a new lowest if the room is vistable by a guard
         if g.dist[r.r][r.c] == 0 {
             g.dist[r.r][r.c] = low + 1
         } else if low < g.dist[r.r][r.c] {
@@ -137,6 +162,7 @@ func (g *grid) calcDistance(guard room, r room, visited map[room]bool) {
         }
 	}
 	visited[r] = true
+    fmt.Println("going to visit", neighbors, "from", r)
 	g.calcDistances(guard, neighbors, visited)
 }
 func abs(n int) int {
@@ -148,7 +174,7 @@ func abs(n int) int {
 
 func main() {
 	fmt.Println("hello")
-	g := newGrid(5, 5)
+	g := newGrid(6, 6)
 	g.placeGuard(room{3, 3})
 	g.placeGuard(room{2, 4})
 	g.placeGuard(room{1, 1})
